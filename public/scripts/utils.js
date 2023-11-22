0aaa4dd8fe0ddd3055cfa69d32a18eb74d9b2284
Shanthanu Nagesh <shanthanucn@gmail.com>
2023-11-22 18:56:09 +0530

    removed offline from phone mode

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -214,3 +214,19 @@ const performAction = (data) => {
       opponent.keys.block = false;
   }
 };
+
+const detectMobile = () => {
+  if (
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
